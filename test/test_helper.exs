c0f872e5b8f62920afd75cbe0d31f08097352868
Dickson <dicksonmelo@gmail.com>
2022-08-25 05:04:38 -0300

    setting up tests

---
+++ test/test_helper.exs
@@ -0,0 +1,5 @@
+ExUnit.start()
+{:ok, _} = Application.ensure_all_started(:wallaby)
+Application.put_env(:wallaby, :base_url, ChatterWeb.Endpoint.url())
+# concurrent tests
+Ecto.Adapters.SQL.Sandbox.mode(Chatter.Repo, :manual)
