3da01d17fb51863c7fbeba516de8b3c8240dee59
Dickson <dicksonmelo@gmail.com>
2022-08-26 05:47:30 -0300

    test: refactoring some tests and removing unused changeset

--- test/chatter_web/features/user_visits_homepage_test.exs
+++ test/chatter_web/features/user_visits_homepage_test.exs
@@ -12,10 +12,14 @@ defmodule ChatterWeb.Features.UserVisitsHomepageTest do
 
   test "user visits rooms page to see a list of rooms", %{session: session} do
     # maps to chat_room_factory function
-    room = insert(:chat_room)
+    [room1, room2] = insert_pair(:chat_room)
 
     session
-    |> visit(Routes.chat_room_path(@endpoint, :index))
-    |> assert_has(Query.css(".room", text: room.name))
+    |> visit(rooms_index())
+    |> assert_has(room_name(room1))
+    |> assert_has(room_name(room2))
   end
+
+  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+  defp room_name(room), do: Query.data("role", "room", text: room.name)
 end
